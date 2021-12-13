ecee9d1caf6689c30209c7ea7019729272e84ca3
Chan Myung Park <cmpark@riotgames.com>
2021-12-13 13:56:29 -0800

    completed day4 part 2

--- day4/main.py
+++ day4/main.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 class Bingo: 
     def __init__(self, board): 
         self.board: List[List[int]] = board
-        self.win: bool = False 
+        self.won: bool = False 
         self.marks: List[Tuple[int, int]] = [] # coordinates of marks (row, col)
 
     # checks to see if mark is on board and adds coordinates to marks
@@ -25,7 +25,7 @@ class Bingo:
                     break
                 # if entire column is in marks; return true 
                 if row == size - 1: 
-                    self.win = True 
+                    self.won = True 
                     return True
 
         # check rows
@@ -35,7 +35,7 @@ class Bingo:
                     break
                 # if entire row is in marks; return true 
                 if col == size - 1: 
-                    self.win = True 
+                    self.won = True 
                     return True
 
         # if no row or col is found; return false 
@@ -44,38 +44,33 @@ class Bingo:
 class Game: 
     def __init__(self): 
         self.games: List[Bingo] = []
-        self.winning_board: Bingo = None 
+        self.last_won_board: Bingo = None # the last winning board
         self.last_draw: int = 0 
+        self.num_games: int = 0 # counts the number of boards in play 
+        self.num_won_boards: int = 0 # counts the number of boards that have won 
 
     def add_board(self, board: Bingo) -> None: 
         self.games.append(board)
+        self.num_games += 1 
 
     # adds draw; returns true if a board has won else false
-    def draw(self, number: int) -> bool: 
+    def draw(self, number: int) -> None: 
         self.last_draw = number
         for game in self.games: 
-            game.add_mark(number)
+            if not game.won:
+                game.add_mark(number)
 
-            if game.check_win(): 
-                self.winning_board = game
-                return True
-
-        return False
+                if game.check_win(): 
+                    self.last_won_board = game
+                    self.num_won_boards += 1
     
     def calculate_win(self) -> int: 
         result = 0
-        for row_i, row_v in enumerate(self.winning_board.board): 
+        for row_i, row_v in enumerate(self.last_won_board.board): 
             for col_i, col_v in enumerate(row_v): 
-                if (row_i, col_i) not in self.winning_board.marks:
+                if (row_i, col_i) not in self.last_won_board.marks:
                     result += col_v
-
         return result * self.last_draw
-
-"""
-def calculate_unmarked_sum(winning_board: Bingo, mark: int) -> int: 
-    for row in winning_board.board: 
-        for col in row: 
-"""
 
 def play_game(boards, draws) -> int: 
 
@@ -85,10 +80,11 @@ def play_game(boards, draws) -> int:
     for board in boards: 
         bingo_game.add_board(Bingo(board))
 
-    # go through draws until a board has won 
+    # go through draws until all boards have won 
     for draw in draws: 
-        won = bingo_game.draw(draw)
-        if won: 
+        print(draw)
+        bingo_game.draw(draw)
+        if bingo_game.num_games == bingo_game.num_won_boards: 
             break
 
     return bingo_game.calculate_win()
