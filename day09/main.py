cbc939a9df0374361203df3fe9815b9a6bfdfa48
Chan Myung Park <cmpark@riotgames.com>
2021-12-28 17:30:02 -0800

    complete day 09

--- day09/main.py
+++ day09/main.py
@@ -8,6 +8,7 @@ def printMap(map):
 
 def findLows(map): 
     low = []
+    ans = 0 
 
     for i in range(len(map)): 
         for j in range(len(map[i])):
@@ -30,11 +31,28 @@ def findLows(map):
                     p += map[i+1][col_start:col_end]
                 if map[i][j] == min(p): 
                     low.append((i,j))
+                    ans += map[i][j] + 1 
 
-    return low 
+    return (low, ans)
+
+def findBasins(point, map, basin): 
+    DR = [1, 0, -1, 0]
+    DC = [0, 1, 0, -1]
+    r = point[0]
+    c = point[1]
+    for k in range(4): 
+        rr = r + DR[k]
+        cc = c + DC[k]
+        if rr >= 0 and cc >= 0 and rr < len(map) and cc < len(map[0]): 
+            if map[rr][cc] < 9: 
+                if (rr, cc) not in basin: 
+                    basin.append((rr, cc))
+                    findBasins((rr, cc), map, basin)
+    
+    return basin
 
 if __name__ == "__main__": 
-    f = "test.txt"
+    f = "input.txt"
 
     with open(f, "r") as input: 
         lines = [ n for n in input.read().strip().split("\n")]
@@ -43,4 +61,19 @@ if __name__ == "__main__":
     for line in lines: 
         map.append([ int(n) for n in list(line) ])
 
-    print(findLows(map))
+    L = findLows(map)
+    # print(f"part one ans: {L[1]}")
+    lows = L[0]
+
+    max_basins = []
+    for low in lows: 
+        b = findBasins(low, map, [low])
+        max_basins.append(len(b))
+        max_basins.sort(reverse=True)
+        max_basins = max_basins[:3]
+    
+    ans2 = 1
+    for n in max_basins: 
+        ans2 *= n
+    
+    print(f"part two ans: {ans2}")
